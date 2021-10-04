3186549656444784198e993ccb8013954e937cf3
Rodrigo Canario <rodrigocanario@MacBook-Pro-de-Rodrigo.local>
2021-10-04 16:52:19 -0300

    Deploy Padrao

--- src/Redux/Reducers/errors.js
+++ src/Redux/Reducers/errors.js
@@ -2,8 +2,6 @@ const reducerErrors = (
   state = { login: false, pedidoMinimo: false },
   action
 ) => {
-  console.log(action.key);
-  console.log(action.value);
   switch (action.type) {
     case "SETERROR":
       return { ...state, [action.key]: action.value };
