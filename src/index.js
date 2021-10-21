38d2d8959919e99e834d865350d85c6d048e22c4
Rodrigo Canario <rodrigocanario@MacBook-Pro-de-Rodrigo.local>
2021-10-21 16:15:26 -0300

    Deploy Padrao

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ import allReducers from "./Redux/Reducers";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-let store = createStore(allReducers, compose(applyMiddleware(thunk)));
+let store = createStore(
+  allReducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
